23f6c913a9ec50a06a0ed2eecdab863da2f33c2c
redmejia <preynaldorod@gmail.com>
2022-03-16 16:37:54 -0700

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -5,6 +5,7 @@ import {
 } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 
 class Reservation extends Component {
@@ -20,6 +21,14 @@ class Reservation extends Component {
         };
     }
 
+    resetForm() {
+        this.setState({
+            campers: 1,
+            hikeIn: false,
+            date: new Date(),
+            showCalendar: false,
+        });
+    }
 
     static navigationOptions = {
         title: 'Reserve Campsite'
@@ -41,8 +50,11 @@ class Reservation extends Component {
                     onPress: () => console.log('cancel pressed')
                 },
                 {
-                    text: 'Ok',
-                    onPress: () => console.log('ok pressed')
+                    text: 'OK',
+                    onPress: () => {
+                        this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                        this.resetForm();
+                    }
                 }
             ]
 
@@ -50,17 +62,36 @@ class Reservation extends Component {
         )
     }
 
-    resetForm() {
-        this.setState({
-            campers: 1,
-            hikeIn: false,
-            date: new Date(),
-            showCalendar: false,
-        });
+    
+
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
     }
 
     render() {
-        return (
+        return ( 
             <ScrollView>
                 <Animatable.View
                     animation="zoomIn"
